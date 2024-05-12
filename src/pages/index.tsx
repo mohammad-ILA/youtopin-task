5f6cc0b3f3d2736eb5dc129ec4f86d02e2eeae2a
mohammad-ILA <mghazim93@gmail.com>
2024-05-12 11:37:00 +0330

    implement todo get and post functions to connect to the server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,46 @@
 import TodoList from "@/components/todo-list";
 import WriteTodo from "@/components/write-todo";
+import { getTodosList, postTodo } from "@/services/todos";
+import { TodoType } from "@/services/todos/index.types";
 import { Box, Divider, Typography } from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  useEffect(() => {
+    getTodoListHandler();
+  }, []);
+  async function getTodoListHandler() {
+    try {
+      const todos = await getTodosList();
+      setTodos(todos);
+    } catch (err) {}
+  }
+  const addTodo = useCallback(async function (
+    todo: TodoType,
+    callback?: () => void
+  ) {
+    try {
+      await postTodo(todo);
+      callback?.();
+      getTodoListHandler();
+    } catch (err) {}
+  },
+  []);
+
   return (
     <Box width="600px" mx="auto">
       <Typography my={2} variant="h4" fontWeight="bold">
         Write a todo
       </Typography>
-      <WriteTodo />
+      <WriteTodo addTodo={addTodo} />
       <Divider />
       <Typography mt={2} variant="h4" fontWeight="bold">
         Todos
       </Typography>
-      <TodoList />
+      {todos.map((todo) => (
+        <TodoList todo={todo} key={todo.id} />
+      ))}
     </Box>
   );
 }
