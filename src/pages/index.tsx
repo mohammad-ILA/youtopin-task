ef82ad08db3f571d44d1c99b9d8e0e700b48caef
mohammad-ILA <mghazim93@gmail.com>
2024-05-12 10:55:38 +0330

    implement todo ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
+import TodoList from "@/components/todo-list";
+import WriteTodo from "@/components/write-todo";
+import { Box, Divider, Typography } from "@mui/material";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Box width="600px" mx="auto">
+      <Typography my={2} variant="h4" fontWeight="bold">
+        Write a todo
+      </Typography>
+      <WriteTodo />
+      <Divider />
+      <Typography mt={2} variant="h4" fontWeight="bold">
+        Todos
+      </Typography>
+      <TodoList />
+    </Box>
+  );
 }
